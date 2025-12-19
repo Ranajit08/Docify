6275b25e288d2a10cfeb7bbf5981fafe7cbca2d2
Ranajit Mitra <creativeranajit12@gmail.com>
2025-12-19 17:30:41 +0530

    working server ready

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,27 @@
 from flask import Flask
-from .extensions import db
+from .extensions import db, migrate
 import os
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='docify',
         SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(app.instance_path, 'db.sqlite'),
         SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
     try:
         os.makedirs(app.instance_path)
+        os.makedirs("storage", exist_ok = True)
+        os.makedirs("storage/users", exist_ok = True)
     except OSError:
         pass
     
     db.init_app(app)
+    migrate.init_app(app, db)
+    
+    @app.route("/")
+    def home():
+        return "hello World from gargi"
     
     # register blueprints
 
