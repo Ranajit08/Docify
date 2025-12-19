fccc8c206f9948d70d45218e08d122512f77ab1d
Ranajit Mitra <creativeranajit12@gmail.com>
2025-12-19 14:39:06 +0530

    made production ready

---
+++ tools/docify.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int converter(char c, char i, char o);
+
+int main(void) {
+    printf("all right\n");
+    return 0;
+}
+
+int converter(char c, char i, char o){
+    return 0;
+}
